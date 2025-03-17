60d61368bd8edfb4000913ed246cd800b481d523
Yousif Dahabra <yousif.dahabra@nsquared.io>
2025-03-17 21:44:32 +0200

    init workflow --fix

--- external/report-builder/deploy.sh
+++ external/report-builder/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "deploying to Google Cloud Run..."
-echo "Using PROJECT=$PROJECT_ID"
+echo "Using PROJECT=$PROJECT"
 
 gcloud run deploy my-service \
     --source . \
     --region us-central1 \
-    --project=$PROJECT_ID \
+    --project=$PROJECT \
     --port 443 \
     --allow-unauthenticated \
     --concurrency=1 \
@@ -15,6 +15,6 @@ gcloud run deploy my-service \
     --memory 12Gi \
     --cpu 4 \
     --service-account=service-workflow-test@test-cloud-453720.iam.gserviceaccount.com \
-    --set-env-vars=GH_TOKEN="$GH_TOKEN",CLIENT_EMAIL="$CLIENT_EMAIL",PROJECT_ID="$PROJECT_ID",PRIVATE_KEY="$PRIVATE_KEY"
+    --set-env-vars=GH_TOKEN="$GH_TOKEN",CLIENT_EMAIL="$CLIENT_EMAIL",PROJECT_ID="$PROJECT",PRIVATE_KEY="$PRIVATE_KEY"
 
 echo "deployment successful!"
