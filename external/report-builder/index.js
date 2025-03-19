b575e23d63ecb86f0f4100e785a31920fde09955
Yousif Dahabra <yousif.dahabra@nsquared.io>
2025-03-19 14:09:53 +0200

    init workflow --fix

--- external/report-builder/index.js
+++ external/report-builder/index.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello from blinkmetrics-report-builder!');
+  res.send('Hello from blinkmetrics-report-builder v2!');
 });
 
 app.listen(port, () => {
